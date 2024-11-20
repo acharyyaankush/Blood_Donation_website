da6e306788a41083e949e9948b3109ca5d5df54b
Ankush Acharyya <156563735+acharyyaankush@users.noreply.github.com>
2024-11-20 18:02:35 +0530

    Add files via upload

---
+++ frontend/src/Footer1.js
@@ -0,0 +1,19 @@
+import './App.css'
+function Footer1() {
+    return (
+        <>
+            <footer>
+                
+                <div className='copy'>
+                    <div class="copyright" style={{ padding: "8px", fontSize: "18px"}}>
+                        Designed & Developed by <span style={{color: "red"}}>Ankush Acharyya</span>
+                    </div>
+                    <div class="copyright" style={{ padding: "8px", fontSize: "18px"}}>
+                        Copyright&#169; <span style={{color: "red"}}>BloodDonor Website</span> || All rights reserved
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
+export default Footer1;
---
+++ frontend/src/HomeSlider.js
@@ -0,0 +1,14 @@
+function HomeSlider() {
+    return (
+        <>
+           <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://myshpl.com/website-assets/images/blood-donation.jpg" alt="First slide"/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default HomeSlider;
